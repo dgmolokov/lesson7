239776d2dd428c3425138a5d5b1d3c39cc4f2cd9
dgmolokov <dg.molokov@gmail.com>
2023-03-01 13:43:25 +0700

    created cats list with List.of method

--- src/lesson7/Main.java
+++ src/lesson7/Main.java
@@ -1,25 +1,24 @@
 package lesson7;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
     var plate = new Plate(100);
-    var cats = new Cat[]{
+    var cats = List.of(
       new Cat("Barsik", 5, false),
       new Cat("Murzik", 7, false),
       new Cat("Huilusha", 10, false),
       new Cat("Eva", 100500, false)
-    };
-    var catsList = Arrays.asList(cats);
-    for (Cat cat : catsList) {
+    );
+    for (Cat cat : cats) {
       cat.eat(plate);
       System.out.printf("%s satiety: %s\n", cat.getName(), cat.isWellFed());
     }
     System.out.println(plate.toString());
     System.out.println("----------");
     plate.increaseFood(200000);
-    for (Cat cat : catsList) {
+    for (Cat cat : cats) {
       cat.setWellFed(false);
       cat.eat(plate);
       System.out.printf("%s satiety: %s\n", cat.getName(), cat.isWellFed());
